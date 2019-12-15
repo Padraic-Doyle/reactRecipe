9a65b6c0cdc4d42b842cd6664da5b3511726986d
padraic <high5developer@gmail.com>
2019-12-15 12:23:57 +0100

    Updates

---
+++ precache-manifest.8ad243938790ea9b162f31a7780077e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ad97a8e8c480d34282826755dbcedd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65f7db85bb31bce3dec",
+    "url": "/static/css/main.1238ae2f.chunk.css"
+  },
+  {
+    "revision": "781afdc5c5fdb751ef85",
+    "url": "/static/js/2.881efebe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.881efebe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c65f7db85bb31bce3dec",
+    "url": "/static/js/main.b267628e.chunk.js"
+  },
+  {
+    "revision": "865aff815f67c30475eb",
+    "url": "/static/js/runtime-main.04974109.js"
+  }
+]);
